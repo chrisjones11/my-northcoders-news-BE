7d7c60ea7818513aba55a63ef4c1d39536ff2d52
jonesyk28 <jonesyk28@gmail.com>
2017-10-28 22:15:16 +0100

    implement fetchAllTopics, fetchArticlesByTopic, fetchCommentsFromArticle and test respones with postman

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router(); 
-const {fetchAllTopics, fetchTopicsById, fetchAllArticles, fetchCommentsFromArticle, addCommentToArticle, changeArticleVoteByOne, changeCommentVoteByOne, deleteComment, fetchUser} = require('../controllers/index');
+const {fetchAllTopics, fetchArticlesByTopic, fetchAllArticles, fetchCommentsFromArticle, addCommentToArticle, changeArticleVoteByOne, changeCommentVoteByOne, deleteComment, fetchUser} = require('../controllers/index');
 
 
 router.get ('/articles', fetchAllArticles);
 
 router.get ('/topics', fetchAllTopics);
 
-router.get ('/topics/:topic/articles', fetchTopicsById);
+router.get ('/topics/:topic/articles', fetchArticlesByTopic);
 
 router.get ('/articles/:article_id/comments', fetchCommentsFromArticle);
 
