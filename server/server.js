a4b9e5d103edb49a9643790232ddad5d2e94da4f
jonesyk28 <jonesyk28@gmail.com>
2017-11-17 15:53:45 +0000

    add static html file

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
+var cors = require('cors');
 var config = require('./config');
 var db = config.DB[process.env.NODE_ENV] || process.env.DB;
 var router = require('./routes/index');
@@ -12,6 +13,9 @@ mongoose.Promise = global.Promise;
 mongoose.connect(db, {useMongoClient: true})
     .then(() => console.log('successfully connected to', db))
     .catch(err => console.log('connection failed', err));
+
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
