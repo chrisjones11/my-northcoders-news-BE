16e5a8f7c8c0aec3d657349f4c00d860ceab8407
jonesyk28 <jonesyk28@gmail.com>
2017-11-18 17:37:15 +0000

    add mlab database to .env file

--- server/config.js
+++ server/config.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 module.exports = {
     DB: {
         test: 'mongodb://localhost/northcoders-news-api-test',
-        dev: process.env.MONGO_URI || 'mongodb://localhost/northcoders-news-api'
+        dev: 'mongodb://chrisj:1234@ds113586.mlab.com:13586/northcoders-news-api' || 'mongodb://localhost/northcoders-news-api'
     },
     PORT: {
         test: 3090,
-        dev: process.env.PORT
+        dev: process.env.PORT || 5000
     }
 };
