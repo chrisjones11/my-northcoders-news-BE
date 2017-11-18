ba7090a7134fc48a0b50517d9b6a3e36ddc128f4
jonesyk28 <jonesyk28@gmail.com>
2017-11-18 20:36:38 +0000

    connect to mlab database and seed

--- server/config.js
+++ server/config.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 module.exports = {
     DB: {
         test: 'mongodb://localhost/northcoders-news-api-test',
-        dev: 'mongodb://chrisj:1234@ds113586.mlab.com:13586/northcoders-news-api' || 'mongodb://localhost/northcoders-news-api'
+        dev:   'mongodb://localhost/northcoders-news-api',
+        production: 'mongodb://chrisj:1234@ds113586.mlab.com:13586/northcoders-news-api'
     },
     PORT: {
         test: 3090,
         dev: process.env.PORT || 5000,
-        production:process.env.PORT
+        production: process.env.PORT
     }
 };
