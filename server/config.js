155070130dfc953ba4633d36d8c20ad70950fa9d
jonesyk28 <jonesyk28@gmail.com>
2017-11-18 21:01:50 +0000

    connect to heroku

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ module.exports = {
     DB: {
         test: 'mongodb://localhost/northcoders-news-api-test',
         dev:   'mongodb://localhost/northcoders-news-api',
-        production: process.env.MONGO_URI
+        production: process.env.MONGO_URI || 'mongodb://localhost/northcoders-news-api'
     },
     PORT: {
         test: 3090,
